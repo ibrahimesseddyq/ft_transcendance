fa460ffbbf06f91e892e401bf0760e30a2afabbd
Chidori42 <elfagrouch9@gmail.com>
2025-12-17 13:43:19 +0100

    change the old front to new, begine with login page

--- frontend/src/pages/Loginpage.tsx
+++ frontend/src/pages/Loginpage.tsx
@@ -13,38 +13,38 @@ export function LoginPage(){
         navigate('/homepage'); 
     };
 
-    const getTabClasses = (tabName: 'signin' | 'signup') => {
+    const getTabClasses = (tabName: String) => {
         const baseClasses = "h-[90%] w-[50%] rounded-full flex items-center justify-center transition-colors duration-300";
         return activeAuth === tabName 
             ? `${baseClasses} bg-[#44BC19] m-1` 
             : `${baseClasses} bg-transparent m-1`; 
     };
     
-    const getTextColor = (tabName: 'signin' | 'signup') => {
+    const getTextColor = (tabName: String) => {
         return activeGender === tabName ? 'text-white' : 'text-[#A8A2A2]';
     };
-    const getGender = (tabName: 'Male' | 'Female') => {
+    const getGender = (tabName: String) => {
         const baseClasses = "h-[90%] w-[50%] rounded-full flex items-center justify-center transition-colors duration-300";
         return activeGender === tabName 
             ? `${baseClasses} bg-[#405673] m-1` 
             : `${baseClasses} bg-transparent m-1`; 
     };
     
-    const getGenderTextColor = (tabName: 'Male' | 'Female') => {
+    const getGenderTextColor = (tabName: String) => {
         return activeAuth === tabName ? 'text-white' : 'text-[#A8A2A2]';
     };
 
     const Signin = () => {
         return(
 
-            <div className="p-1 flex flex-col w-full h-full gap-2 items-center">
+            <div className="p-1 flex flex-col w-full h-full gap-2 items-center my-4">
                 <div className="w-full ml-4">
                     <h2 className="text-[#FFCE22] font-electrolize text-sm ">Welcome Back!</h2>
                     <h1 className="text-md font-electrolize text-white">
                         We are happy to see you again. 
                     </h1>
                 </div>
-                <div className='flex mt-5 h-[40px] w-[90%] border border-[#405673] rounded-full bg-transparent items-center justify-between'>
+                <div className='flex mt-3 h-[40px] w-[90%] border border-[#405673] rounded-full bg-transparent items-center justify-between'>
                         <button 
                             onClick={() => {setActiveAuth('signin'); setRetate(true);}}
                             className={getTabClasses('signin')}
@@ -58,29 +58,30 @@ export function LoginPage(){
                             <h1 className={`text-sm font-bold ${getTextColor('signup')}`}>Sign Up</h1>
                         </button>
                 </div>
-                <div className="flex flex-col w-[90%]  gap-5 mt-5">
-                    <div className="flex justify-between items-center h-[50px] w-full border-transparent border border-[#415876] bg-transparent rounded-full">
+                <div className="flex flex-col w-[90%] ">
+                    <div className="flex justify-between items-center h-[50px] w-full mt-5 border-transparent border border-[#afb8c4] rounded-full">
                         <input
                             placeholder="Enter your email"
                             type="text"
-                            className="w-full h-full outline-none placeholder-white pl-5 bg-transparent"
+                            className="w-full h-full text-white outline-none placeholder-white pl-5 bg-transparent"
                         />
                         <Mail className="h-10 w-14 pr-5 text-white" />
                     </div>
-                    <div className="flex justify-between items-center h-[50px] w-full border-transparent border border-[#415978] bg-transparent rounded-full">
+                    <div className="flex justify-between items-center h-[50px] w-full mt-3 border-transparent border border-[#afb8c4]  rounded-full">
                         <input
                             placeholder="Enter your Password"
                             type="text"
-                            className="w-full h-full  placeholder-white pl-5 bg-transparent"
+                            className="w-full h-full text-white outline-none placeholder-white pl-5 bg-transparent"
                         />
                         <Eye className="h-10 w-14 pr-5 text-white" />
                     </div>
+                    <a href='#' className='text-[#44BC19] text-sm font-semibold hover:underline hover:cursor-pointer w-28'>Forgot Password?</a>
                     <button onClick={handlePlayClick}
-                            className="text-white font-bold w-full mx-auto h-[50px]  border rounded-full bg-[#44BC19]">
+                            className="text-white font-bold w-full mx-auto h-[50px] mt-5 border rounded-full bg-[#44BC19]">
                         Log in
                     </button>
                     <button onClick={handlePlayClick}
-                            className="h-[50px]  flex gap-5 rounded-full w-full mx-auto border border-[#405673] bg-transparent text-white hover:text-black hover:bg-white items-center">
+                            className="h-[50px]  flex gap-5 rounded-full w-full mt-5 mx-auto border border-[#405673] bg-transparent text-white hover:text-black hover:bg-white items-center">
                         <img    className="h-8 w-8 ml-10" 
                                 src="src/assets/icons/google1.png"
                                 alt="Google icon"/>
@@ -92,14 +93,14 @@ export function LoginPage(){
     }
     const Signup = () => {
         return(
-            <div className="p-1 flex flex-col w-full h-full gap-1 items-center">
+            <div className="p-1 flex flex-col w-full h-full gap-1 items-center my-4">
                 <div className="w-full ml-4">
                     <h2 className="text-[#FFCE22] font-electrolize text-sm ">Welcome!</h2>
                     <h1 className="text-md font-electrolize text-white">
                         We are happy to have you. 
                     </h1>
                 </div>
-                <div className='flex mt-5 h-[40px] w-[90%] border border-[#405673] rounded-full bg-transparent items-center justify-between'>
+                <div className='flex mt-3 h-[40px] w-[90%] border border-[#405673] rounded-full bg-transparent items-center justify-between'>
                         <button 
                             onClick={() => {setActiveAuth('signin'); setRetate(true);}}
                             className={getTabClasses('signin')}
@@ -117,21 +118,21 @@ export function LoginPage(){
                     <div className='flex gap-4 h-[40px] w-full '>
                         <input
                             placeholder="First Name"
-                            className=" w-full lg:w-[60%] mx-auto pl-3 border  border-[#405673] rounded-full  bg-transparent"
+                            className=" w-full lg:w-[60%] text-sm text-white outline-none placeholder-white mx-auto pl-3 border  border-[#405673] rounded-full  bg-transparent"
                         />
                         <input
                             placeholder="Last Name"
-                            className=" w-full lg:w-[60%] mx-auto pl-3 border border-[#405673] rounded-full  bg-transparent"
+                            className=" w-full lg:w-[60%] text-sm text-white outline-none placeholder-white mx-auto pl-3 border border-[#405673] rounded-full  bg-transparent"
                         />
                     </div>
                     <div className='flex gap-4 h-[40px] w-full'>
                         <input
                             placeholder="Category"
-                            className=" w-full lg:w-[60%] mx-auto pl-3 border  border-[#405673] rounded-full  bg-transparent"
+                            className=" w-full lg:w-[60%] text-sm text-white outline-none placeholder-white mx-auto pl-3 border  border-[#405673] rounded-full  bg-transparent"
                         />
                         <input
                             placeholder="Number"
-                            className="w-full lg:w-[60%] mx-auto pl-3 border border-[#405673] rounded-full  bg-transparent"
+                            className="w-full lg:w-[60%] text-sm text-white outline-none placeholder-white mx-auto pl-3 border border-[#405673] rounded-full  bg-transparent"
                         />
                     </div>
                     <div className='flex h-[40px] w-full border border-[#405673] rounded-full bg-transparent items-center justify-between'>
@@ -139,22 +140,22 @@ export function LoginPage(){
                             onClick={() => {setActiveGender('Male'); setRetate(true);}}
                             className={getGender('Male')}
                             >
-                            <h1 className={`text-sm font-bold ${getGenderTextColor('Male')}`}>Sign In</h1>
+                            <h1 className={`text-sm font-bold ${getGenderTextColor('Male')}`}>Male</h1>
                         </button>
                         <button 
                             onClick={() => {setActiveGender('Female'); setRetate(false);}}
                             className={getGender('Female')}
                         >
-                            <h1 className={`text-sm font-bold ${getGenderTextColor('Female')}`}>Sign Up</h1>
+                            <h1 className={`text-sm font-bold ${getGenderTextColor('Female')}`}>Female</h1>
                         </button>
                     </div>
                     <input
                             placeholder="Enter Your Email"
-                            className="h-[40px] w-full mx-auto pl-3 border border-[#405673] rounded-full  bg-transparent"
+                            className="h-[40px] w-full text-sm text-white outline-none placeholder-white mx-auto pl-3 border border-[#405673] rounded-full  bg-transparent"
                         />
                     <input
                             placeholder="Enter Your Password"
-                            className="h-[40px] w-full mx-auto pl-3 border  border-[#405673] rounded-full  bg-transparent"
+                            className="h-[40px] w-full text-sm text-white outline-none placeholder-white mx-auto pl-3 border  border-[#405673] rounded-full  bg-transparent"
                         />
                     <button onClick={handlePlayClick}
                             className="text-white font-bold w-full mx-auto h-[40px] border rounded-full bg-[#44BC19]">
@@ -179,7 +180,7 @@ export function LoginPage(){
     }`;
     
     return(
-        <div className={`w-full h-[700px] lg:h-[457px] lg:w-[441px] p-4 lg:p-0 border border-[#5F88B8] rounded-lg bg-[#09122C] mx-auto flex items-center justify-center overflow-hidden`}>
+        <div className={`h-auto w-full sm:p-5 sm:w-[441px] p-0 border border-[#5F88B8] rounded-lg bg-[#09122C] mx-auto flex items-center justify-center overflow-hidden`}>
             <div className="flex-grow  h-full w-full">
                 {activeAuth === 'signin' ? <Signin /> : <Signup />}
             </div>
