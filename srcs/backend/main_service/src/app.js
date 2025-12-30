2464003fe312209c057c7ecbbd4ebcaa8c96977b
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-12-30 15:50:17 +0100

    start prisma connection implementation

--- srcs/backend/main_service/src/app.js
+++ srcs/backend/main_service/src/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
- 
+const bodyParser = require('body-parser');
+const app =  express();
+
+const PORT = process.env.MAIN_SERVICE_PORT || 3000;
 
 
+app.listen(PORT, () => {
 
-
+});
