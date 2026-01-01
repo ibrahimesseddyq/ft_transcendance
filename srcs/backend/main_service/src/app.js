f2af1c4397102e7396b245a66b900c4ed150e34b
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-01 13:37:42 +0100

    [@sessarhi] :  create prisma client instance and start implemeting users CRUD api logic

--- srcs/backend/main_service/src/app.js
+++ srcs/backend/main_service/src/app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app =  express();
 
-const PORT = process.env.MAIN_SERVICE_PORT || 3000;
 
 
-app.listen(PORT, () => {
-
-});
+module.exportes = app; 
