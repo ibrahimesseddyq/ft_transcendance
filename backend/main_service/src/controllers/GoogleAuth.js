8e55b842065fdd063f163324c3327ad54f2a2928
Chidori42 <elfagrouch9@gmail.com>
2026-01-01 06:04:56 +0100

    finish google auth

---
+++ backend/main_service/src/controllers/GoogleAuth.js
@@ -0,0 +1,70 @@
+const passport = require('passport');
+require('dotenv').config();
+var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../..', '.env') });
+
+// const { PrismaClient } = require('../../generated/prisma');
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+console.log("CALLBACK_URL : *" + process.env.GOOGLE_CLIENT_ID + "*")
+console.log("CALLBACK_URL : *" + process.env.GOOGLE_CLIENT_SECRET + "*")
+console.log("CALLBACK_URL : *" + process.env.CALLBACK_URL + "*")
+passport.use(new GoogleStrategy({ 
+    clientID: process.env.GOOGLE_CLIENT_ID || '103278425538-0iqof4oahn4rfkl1j51tbd4t8bvu6655.apps.googleusercontent.com',
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || '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',
+    callbackURL: 'http://localhost:3000/auth/google/callback'
+  },
+  async function(request, accessToken, refreshToken, profile, done) {
+    try {
+      const nameParts = profile.displayName.split(' ');
+      const firstName = nameParts[0] || '';
+      const lastName = nameParts.slice(1).join(' ') || '';
+
+      const user = await prisma.users.upsert({
+        // search for user with google id
+        where: { id: profile.id },
+        // update the user data if already exist
+        update: {
+          first_name: firstName,
+          last_name: lastName,
+          avatar_url: profile.photos?.[0]?.value || null
+        },
+        // create user if not exist in the database
+        create: {
+          id: profile.id,
+          email: profile.emails?.[0]?.value || '',
+          first_name: firstName,
+          last_name: lastName,
+          avatar_url: profile.photos?.[0]?.value || null,
+          password_hash: '',
+          role: 'candidate'
+        }
+      });
+      
+      return done(null, user);
+    } catch (err) {
+      console.error('Google auth error:', err);
+      return done(err, null);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); // Save only the MySQL ID in the cookie
+});
+
+// Deserialize: Find the actual user from the DB using that ID
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await prisma.users.findUnique({ where: { id } });
+    if (user)
+      done(null, user);
+    else
+      console.log("No user found with this id " + id);
+  } catch (err) {
+    done(err, null);
+  }
+});
+module.exports = passport;
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,53 +1,6 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Home, Gamepad2, MessageCircleMore ,Settings, LogOut, Medal , Award , Trophy  , BrainCircuit, Crown  , Volleyball, Calendar1 } from "lucide-react";
 import {Main} from '@/pages/main'
-
-export const navigation = [
-  { name: "Home", path: "/homepage", icon: Home }, 
-  { name: "Games", path: "/games", icon: Gamepad2}, 
-  { name: "Chat", path: "/chat", icon: MessageCircleMore }, 
-  { name: "Tournaments", path: "/tournaments", icon: Trophy}, 
-  { name: "Settings", path: "/settting", icon: Settings},
-  { name: "LogOut", path: "/logout", icon: LogOut},
-];
-export const matches = [
-  { date: "4/3/22", opponent: "Player A", result: "WIN" },
-  { date: "4/3/22", opponent: "Player B", result: "LOSS" },
-  { date: "4/3/22", opponent: "Player C", result: "DRAW" },
-  { date: "4/3/22", opponent: "Player A", result: "WIN" },
-  { date: "4/3/22", opponent: "Player B", result: "LOSS" },
-  { date: "4/3/22", opponent: "Player C", result: "DRAW" },
-  { date: "4/3/22", opponent: "Player A", result: "WIN" },
-  { date: "4/3/22", opponent: "Player B", result: "LOSS" },
-  { date: "4/3/22", opponent: "Player C", result: "DRAW" },
-  { date: "4/3/22", opponent: "Player A", result: "WIN" },
-];
-
-export const Achive = [
-    { name: "Medal ", icon: Medal , current: true },
-    { name: "Crown", icon: Crown, current: true },
-    { name: "Award ", icon: Award , current: true },
-    { name: "Trophy ", icon: Trophy  , current: true },
-    { name: "Tournaments", icon: Trophy, current: false },
-    { name: "Calendar1", icon: Calendar1, current: false },
-    { name: "Volleyball", icon: Volleyball, current: false },
-    { name: "BrainCircuit", icon: BrainCircuit, current: false },
-
-];
-
-export const Users = [
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "online", matches: matches, Achiv: Achive, totalgames: 10, rank: 1, win: 14, lose: 7 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "online", matches: matches, Achiv: Achive, totalgames: 10, rank: 2, win: 14, lose: 6 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "online", matches: matches, Achiv: Achive, totalgames: 10, rank: 3, win: 14, lose: 6 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "online", matches: matches, Achiv: Achive, totalgames: 10, rank: 4, win: 14, lose: 4 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "offline",matches: matches, Achiv: Achive, totalgames: 10, rank: 5, win: 14, lose: 3 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "offline",matches: matches, Achiv: Achive, totalgames: 10, rank: 6, win: 14, lose: 7 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "offline",matches: matches, Achiv: Achive, totalgames: 10, rank: 7, win: 14, lose: 7 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "offline",matches: matches, Achiv: Achive, totalgames: 10, rank: 8, win: 14, lose: 7 , draw: 3},
-  { name: "CHIDORI", login: "ael-fagr", email: "elfagrouch9@gmail.com", status: "offline",matches: matches, Achiv: Achive, totalgames: 10, rank: 9, win: 14, lose: 7 , draw: 3},
-];
-
 
 
 const queryClient = new QueryClient();
