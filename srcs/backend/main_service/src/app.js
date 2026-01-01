f09eb55608aca6b0c5d80cf61f594f773a60717f
soufianeessarhir <soufianeessarhir@gmail.com>
2026-01-01 20:48:32 +0100

    [@sessarhi] : start implementing the main_service server

--- srcs/backend/main_service/src/app.js
+++ srcs/backend/main_service/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app =  express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-
-module.exportes = app; 
+module.exportes = app;
--- srcs/backend/main_service/src/config/env.js
+++ srcs/backend/main_service/src/config/env.js
@@ -1,7 +1,19 @@
 const dotenv =  require('dotenv');
+const {zod} = require('zod')
 dotenv.config({path: "../../.env.dev"});
 
-module.exports = dotenv;
+const envShcema =  zod.object({
+
+});
+
+const envVars = envShcema.safeParse(process.env);
+if (!envVars.success)
+{
+    console.error(`failed to load the envirment variables`);
+    exit(1);
+}
+
+module.exports = envVars.data;
 
 
 
