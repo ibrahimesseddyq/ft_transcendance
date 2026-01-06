c1591534da94ba7ce5fc53b28455d6c8749caf35
soufianeessarhir <soufianeessarhir@gmail.com>
2026-01-06 18:44:18 +0100

    impelementing job repository

---
+++ srcs/backend/main_service/src/repositories/jobRepository.js
@@ -0,0 +1,57 @@
+const data = require('../config/env');
+const {prisma} = require('../config/prisma');
+
+class JobRepository {
+    async findById(jobId)
+    {
+        return await prisma.jobs.findUnique(
+            {
+                where : {id : jobId},
+                include : {}
+            }
+        )
+    }
+    async create(jobData)
+    {
+        if(!await this.findById(jobData.id))
+        {
+            return await prisma.jobs.create({
+                data : jobData 
+            })
+        }
+    }
+
+    async update(jobId, updateData)
+    {
+        if (await this.findById(jobId))
+        {
+            return await prisma.jobs.update({
+                where : {id : jobId},
+                data: updateData,
+                include : {
+
+                }
+            })
+        }
+        else 
+            return new Error("job does not exists");
+    }
+
+    async delete (jobId)
+    {
+        if (await this.findById(jobId))
+        {
+            return await prisma.jobs.delete(jobId);
+        }
+        else
+            return new Error("job does not exists");
+    }
+
+    async findMany(skip = 0, take = 10)
+    {
+        
+    }
+}
+
+const jobRepository = new JobRepository();
+module.exports = jobRepository;
