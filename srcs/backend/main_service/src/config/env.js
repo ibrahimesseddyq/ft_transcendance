7b5108583d0525ecbf8accc2359dbef3df064cac
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-06 21:44:36 +0100

    fix o'auth

--- srcs/backend/main_service/src/config/env.js
+++ srcs/backend/main_service/src/config/env.js
@@ -1,14 +1,21 @@
-const dotenv =  require('dotenv');
 const { z } = require('zod');
 const path =  require('path');
+const dotenv =  require('dotenv').config({
+  path: path.resolve(__dirname,"../../.env.dev"),
+  override: true
+});
 
-dotenv.config({path: path.resolve(__dirname,"../../.env.dev") });
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']).default('development'),
   PORT: z.string().transform((val) => parseInt(val, 10)).default('3000'),
+  HOST: z.string(),
   DATABASE_URL: z.string(),
-  BCRYPT_ROUNDS:  z.string().transform((val) => parseInt(val, 10)).default('10')
+  BCRYPT_ROUNDS:  z.string().transform((val) => parseInt(val, 10)).default('10'),
+  GOOGLE_CLIENT_ID :z.string(),
+  GOOGLE_CLIENT_SECRET :z.string(),
+  CALLBACK_URL :z.string(),
+
 });
 
 const envVars = envSchema.safeParse(process.env);
