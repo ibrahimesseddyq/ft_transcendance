a96206cf230a178c9d458897c9f5a21232582604
Chidori42 <elfagrouch9@gmail.com>
2026-01-12 02:47:14 +0100

    fix login form with react-hook-form integrated with zod, also change the desing

--- srcs/backend/main_service/src/app.js
+++ srcs/backend/main_service/src/app.js
@@ -14,10 +14,9 @@ const env = require('./config/env');
 
 app.use(helmet());
 app.use(cors({
-    origin: "http://localhost:5173", 
-    credentials: true,           
+    origin: 'http://localhost:5173',
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    credentials: true 
 }));
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({extended:true, limit : "10mb"}));
