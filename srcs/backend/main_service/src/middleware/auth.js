8186b0ad23abc305e94cbd5fc8464fc661c8f206
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-15 12:02:52 +0100

    fix the last merge errors

--- srcs/backend/main_service/src/middleware/auth.js
+++ srcs/backend/main_service/src/middleware/auth.js
@@ -1,7 +1,7 @@
-const {jwt} =  require('jsonwebtoken');
+const jwt =  require('jsonwebtoken');
 const env  = require('../config/env')
 const {HttpException} = require('../utils/httpExceptions');
-const {jwtService} = require('../services/jwtService');
+const jwtService = require('../services/jwtService');
 const { getPermissionsByRole } = require('../config/permissions');
 
 const verifyToken = async (req, res, next) => {
@@ -74,7 +74,7 @@ const verifyOwnership = (req, res, next) =>
     if (!req.user)
         throw new HttpException(401, "Unauthorized");
     const resourceUserId = req.params.id || req.params.userId;
-    if (user.id !== resourceUserId && req.user.role !== 'admin')
+    if (req.user.id !== resourceUserId && req.user.role !== 'admin')
     {
            throw new HttpException(403, 'Forbidden:  You can only access your own resources');
     }
