e6ac34fac3591a5c16bc9e2fbfab778b8b42f272
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-17 18:40:54 +0100

    fix error in register schema validation

--- srcs/backend/main_service/src/config/passport.js
+++ srcs/backend/main_service/src/config/passport.js
@@ -3,28 +3,29 @@ const passport = require('passport');
 const { prisma } = require('./prisma');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
 const userService = require('../services/userService');
-console.log(`http://${env.HOST}:${env.PORT}${env.CALLBACK_URL}`);
 
 const callbackURL = `http://${env.HOST}:${env.PORT}${env. CALLBACK_URL}`;
-passport.use(new GoogleStrategy({
-    clientID: env.GOOGLE_CLIENT_ID,
-    clientSecret: env.GOOGLE_CLIENT_SECRET,
-    callbackURL
-}, async (request, accessToken, refreshToken, profile, done) => {
-    try {
-        const user = await userService.findUserOrCreate(profile);
-        return done(null, user);
-    } catch (err) {
-        return done(err, null);
-    }
-}));
 
-passport.serializeUser((user, done) =>{
+passport.use(new GoogleStrategy({
+        clientID: env.GOOGLE_CLIENT_ID,
+        clientSecret: env.GOOGLE_CLIENT_SECRET,
+        callbackURL
+    }, async (request, accessToken, refreshToken, profile, done) => {
+        try {
+            const user = await userService.findUserOrCreate(profile);
+            return done(null, user);
+        } catch (err) {
+            return done(err, null);
+        }
+    })
+);
+
+passport.serializeUser((user, done) =>  {
     done(null, user.id)
 });
     
 passport.deserializeUser(async (id, done) => {
-    const user = await prisma.users.findUnique({ where: { id } });
+    const user = await userService.getUserById(id);
     done(null, user);
 });
 
