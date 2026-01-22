ab8535f5fa57a9e6fa606494d54d1111ae3caae8
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-22 22:06:06 +0100

    start implementing application logic

--- srcs/backend/main_service/src/repositories/applicationRepository.js
+++ srcs/backend/main_service/src/repositories/applicationRepository.js
@@ -1,3 +1,51 @@
 const prisma =  require('../../generated/prisma');
 
+const createApplication =  async (data) =>
+{
+	return await prisma.application.create({
+		where : {
+			data: data
+		}
+	}
+	)
+}
 
+const deleteApplication =  async (applicationId) => 
+{
+	return await prisma.application.delete({
+		where:{
+			id: applicationId
+		}
+	})
+}
+
+const updateApplication = async (applicationId,updateData) =>
+{
+	return await prisma.application.update({
+		where : {id : applicationId},
+		data:updateData
+	})
+}
+
+const getApplicaticationById = async (appliocationId) =>
+{
+	return await prisma.application.findUnique({
+		where:{ id : appliocationId}
+	})
+}
+
+const getApplicatications = async (skip = 0, take = 10, filters= []) =>
+{
+	return await prisma.findMany({
+
+	})
+}
+
+
+module.exports = {
+	createApplication,
+	getApplicatications,
+	getApplicaticationById,
+	updateApplication,
+	deleteApplication
+}
---
+++ srcs/backend/main_service/src/services/applicationService.js
@@ -0,0 +1,1 @@
+const applicationRepository = require('../repositories/applicationRepository');
