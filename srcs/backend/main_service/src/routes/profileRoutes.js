e6fc00300d392cd2954890791de1bf51e1878de5
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-29 21:48:39 +0100

    update userRoutes to supporting avatar

--- srcs/backend/main_service/src/routes/profileRoutes.js
+++ srcs/backend/main_service/src/routes/profileRoutes.js
@@ -1,11 +1,12 @@
 const profileController = require('../controllers/profileController');
 const express =  require('express');
 const router = express.Router();
+const upload =  require('../config/multer');
 
 
-router.get('/:id');
-router.post('/');
-router.patch('/:id');
+router.get('/:id', profileController.getProfile)
+    .post('/',upload.single("resume"),profileController.createProfile)
+    .patch('/:id',upload.single("resume"),profileController.updateProfile);
 
 module.exports =  {
     router
--- srcs/backend/main_service/src/routes/userRoutes.js
+++ srcs/backend/main_service/src/routes/userRoutes.js
@@ -1,4 +1,5 @@
 const express =  require('express');
+const upload = require('../config/multer')
 const userController = require('../controllers/userController');
 const ValidateRequest = require('../middleware/ValidateRequest');
 const {
@@ -8,9 +9,12 @@ const {
 const router =  express.Router();
 
 router.get('/',userController.listUsers)
-    .get('/:id', userController.getUserById)
-    .post('/',ValidateRequest(createUserSchema),userController.createUser)
-    .delete('/:id',userController.deleteUser)
-    .patch('/:id', ValidateRequest(updateUserSchema),userController.updateUser);
+  .get('/:id', userController.getUserById)
+  .post('/',ValidateRequest(createUserSchema),userController.createUser)
+  .delete('/:id',userController.deleteUser)
+  .patch('/:id', ValidateRequest(updateUserSchema),userController.updateUser)
+  .post('/avatar',upload.single("avatar"),userController.uploadAvatar)
+  .get('/avatar/:id',userController.getAvatar)
+  .delete('/avatar/:id',userController.deleteAvatar)
 
 module.exports = router;
