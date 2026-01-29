8fe21231c7d51a9dddfc3712baa75441ea547480
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-01-29 12:21:47 +0100

    complete profile service and controller

--- srcs/backend/main_service/src/services/profileService.js
+++ srcs/backend/main_service/src/services/profileService.js
@@ -4,7 +4,7 @@ const { HttpException } = require('../utils/httpExceptions');
 const fileService = require('./fileService');
 const userService = require('./userService');
 
-const createProfile = async  (profileData, userId) => {
+const createProfile = async  (userId , profileData) => {
     const user = await userService.getUserById(userId);
     if (!user)
         throw new HttpException(404, "user not found");
@@ -16,7 +16,7 @@ const createProfile = async  (profileData, userId) => {
     })
 }
 
-const updateProfile = async (profileData, userId) => {
+const updateProfile = async (userId, profileData) => {
     const user = await userService.getUserById(userId);
     if (!user)
         throw new HttpException(404, 'user not found');
@@ -50,4 +50,31 @@ const deleteProfile = async (userId) => {
     await profileRepository.deleteProfile(userId);
 }
 
-const deleteResume = async ()
+const deleteResume = async (userId) => {
+    const profile = profileRepository.getProfileById(userId);
+    if (!profile)
+        throw new HttpException(404, "profile not found");
+    await fileService.deleteFile(profile.resumeUrl);
+    await profileRepository.updateProfile(userId, {resumeUrl : null});
+}
+
+const updateResume = async (userId, file) => {
+    const profile = await profileRepository.getProfileById(userId);
+    if (!profile)
+        throw new HttpException(404, "profile not found");
+    const {resumeUrl} = fileService.saveResume(userId, file);
+    if (!resumeUrl)
+        throw new HttpException(400, "faild to update resume");
+    if (profile.resumeUrl && profile.resumeUrl !== resumeUrl)
+        await fileService.deleteFile(profile.resumeUrl);
+    return await profileRepository.updateProfile(userId,{resumeUrl});
+}
+
+module.exports = {
+    createProfile,
+    updateProfile,
+    getProfile,
+    deleteProfile,
+    deleteResume,
+    updateResume
+}
