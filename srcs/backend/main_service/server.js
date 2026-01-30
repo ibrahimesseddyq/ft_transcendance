be7cbea961b9c27ac1a467d30f6c71e5769057a9
AM9-push <achllahabdelmajid@gmail.com>
2026-01-30 20:36:59 +0100

    Starting with content moderation for chat I begin with a basic set up of a moderation api I need to modify the response to be accurate and clear, so it can be used easily by the backend (chat) Also an authentication must be added to this moderation api

--- srcs/backend/main_service/server.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/app');
-const config =  require('./src/config/env');
-const {prisma} = require('./src/config/prisma');
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port http://localhost:${config.PORT}`);
-})
