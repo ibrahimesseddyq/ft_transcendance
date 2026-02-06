67354d167656e77a3bda0986f94f1ad985ce0d31
soufiane essarhir <soufianeessarhir@gmail.com>
2026-02-06 12:03:46 +0100

    add create application to application system

--- srcs/backend/main_service/src/services/applicationPhaseService.js
+++ srcs/backend/main_service/src/services/applicationPhaseService.js
@@ -1,37 +1,50 @@
 const applicationPhaseRepository = require('../repositories/applicationPhaseRepository');
 const applicationService = require('./applicationService');
 const jobPhaseService = require('./jobPhaseService');
-const {HttpException} = require('../utils/httpExceptions')
+const {HttpException} = require('../utils/httpExceptions');
 
 const createApplicationphase = async (data) => {
     const {applicationId, phaseId} = data;
-    const application = await applicationService.getApplicaticationById(applicationId);
-    const phase =  await jobPhaseService.getJobPhaseById(phaseId);
-    if (!application)
-        throw new HttpException(404, 'apllication not found');
-    if (!phase)
-        throw new HttpException(404, 'phase not found');
-    return await applicationPhaseRepository.createApplicationPhase({
-        applicationId: applicationId,
-        phaseId:phaseId,
-        interviews: []
-    }) 
+    try {
+        const applicationPhase = await applicationPhaseRepository.createApplicationPhase({
+            applicationId: applicationId,
+            phaseId:phaseId,
+            notes: "",
+            score: 0,
+            interviews: []
+
+        });
+         return applicationPhase;
+    } catch (error) {
+        if (error.code === "P2002")
+            throw new HttpException(409,'application phase already exists');
+        else if (error.code === "P2003")
+            throw new HttpException(404, 'applicatioId or phaseId not valid');
+        else 
+            throw error;
+    }
 }
 
 const updateApplicationPhase = async (applicationPhaseId, updateData) => {
-    const applicationPhase =  await applicationPhaseRepository.getApplicationPhaseById(applicationPhaseId);
-    if (!applicationPhase)
-        throw new HttpException(404, 'application Phase not found');
-    return await applicationPhaseRepository.updateApplicationPhase({
+    const applicationPhase =  await applicationPhaseRepository.updateApplicationPhase({
         applicationPhaseId,
         updateData
-    })
+    });
+    if (!applicationPhase)
+        throw new HttpException(404, 'application Phase not found');
+    return applicationPhase;
 }
 
-const getApplicaticationById =  async (applicationPhaseId) => {
+const getApplicaticationPhaseById =  async (applicationPhaseId) => {
     const applicationPhase =  applicationPhaseRepository.getApplicationPhaseById(applicationPhaseId)
     if (!applicationPhase)
         throw new HttpException(404, "application Phase not found");
     return applicationPhase;
 }
-    
+
+module.exports   =  {
+    createApplicationphase,
+    updateApplicationPhase,
+    getApplicaticationPhaseById
+}
+
