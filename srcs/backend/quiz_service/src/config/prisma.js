3b7fb467d73e2f089186ef44a54d04200c073872
soufiane essarhir <soufianeessarhir@gmail.com>
2026-02-13 19:03:23 +0100

    configure quiz_service to use  prisma 7

--- srcs/backend/quiz_service/src/config/prisma.js
+++ srcs/backend/quiz_service/src/config/prisma.js
@@ -1,10 +1,16 @@
 import { PrismaClient } from '../../generated/prisma';
+import { PrismaMysql } from '@prisma/adapter-mysql';
+import mysql from 'mysql2/promise';
 
 let prismaInstance = null;
 
 const  getPrismaClient = () => {
+
     if (!prismaInstance) {
+        const pool = mysql.createPool(process.env.DATABASE_URL);
+        const adapter = new PrismaMysql(pool);
         prismaInstance = new PrismaClient({
+        adapter,
         log: [],
        errorFormat: 'pretty'
     });
---
+++ srcs/backend/quiz_service/src/services/mcqService.js
@@ -0,0 +1,1 @@
+import * as mcqRepository from '../repositories/mcqRepository';
