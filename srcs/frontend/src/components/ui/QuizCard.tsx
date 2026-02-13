3716508ad2c767d5f72728505380492ef5a4494c
elfagrouch9 <elfagrouch9@gmail.com>
2026-02-14 00:10:03 +0100

    begine with quiz

--- srcs/frontend/src/components/ui/QuizCard.tsx
+++ srcs/frontend/src/components/ui/QuizCard.tsx
@@ -16,9 +16,14 @@ const QuizCard = ({Type}:any) =>{
         return (TrueOrFalse);
     }
     return (
-        <div className="flex flex-col gap-4 justify-between min-h-20 min-w-20 rounded-lg">
-            <h1>{Type}</h1>
-            <p> <Description Type={Type} /></p>
+        <div className={`group flex flex-col gap-4 justify-between min-h-40 min-w-60 
+            rounded-lg hover:duration-500 shadow-md transition duration-300 ease-out
+            bg-slate-200/80 hover:bg-blue-700 p-2 bg-cover bg-center items-center
+            ${Type === "MultipleResponse" ? 'hover:bg-[url(/MultipleResponse.jpg)]':
+                Type === "MultipleChoice" ? 'hover:bg-[url(/MultipleChoice.jpg)]': 
+                'hover:bg-[url(/TrueOrFalse.jpeg)]'}`}>
+            <h1 className="text-xl font-semibold visible group-hover:invisible">{Type}</h1>
+            <p className="text-sm visible group-hover:invisible"> <Description Type={Type} /></p>
             <button className="p-1 px-2 rounded-xl bg-[#00adef] text-white">Create Quiz</button>
         </div>
     );
