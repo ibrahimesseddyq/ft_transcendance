9fae28e19bed12252a6cc9ac1c28f40306ad4699
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-02-13 11:55:30 +0100

    [quiz servise] add proper error handeler for prisma

--- srcs/backend/quiz_service/src/services/testService.js
+++ srcs/backend/quiz_service/src/services/testService.js
@@ -1,7 +1,35 @@
+import { prisma } from '../config/prisma.js';
 import * as testRepository from '../repositories/testRepository.js'
+import {HttpException } from '../utils/httpExceptions.js';
 
-const createTest = async (params) => {
-    
+const createTest = async (testData) => {
+    return await testRepository.createTest(testData);
+}
+
+const updateTest =  async (testId, updateData) => {
+    try {
+        return  await testRepository.updateData(testId,updateData);  
+    } catch (error) {
+        if (error instanceof prisma.PrismaClientKnownRequestError)
+        {
+            if (error.code === "P2025")
+                throw new HttpException(404, "test not found");
+        }
+        throw error
+    }
+}
+
+const getTestById = async (testId) => {
+    try {
+        return await testRepository.getTestById(testId);
+    } catch (error) {
+        if ( error instanceof prisma.PrismaClientKnownRequestError)
+        {
+            if (error.code === 'P2025')
+                throw new HttpException(404, 'test not found');
+        }
+        throw error;
+    }
 }
 
 export {
