9d76d570190dd87c2a21d4d7244bfc4ab352827b
elfagrouch9 <elfagrouch9@gmail.com>
2026-02-15 22:11:59 +0000

    finish quiz test and waiting for merge with backend

--- srcs/frontend/src/components/QuizPage.tsx
+++ srcs/frontend/src/components/QuizPage.tsx
@@ -5,16 +5,18 @@ import TestsList from '@/components/ui/TestsList'
 
 export function QuizPage(){
     return (
-        <div className="grid grid-cols-3 gap-4 overflow-y-auto no-scrollbar items-center w-full  p-2 lg:p-0 ">
-            <div className="col-span-3 lg:col-span-1 bg-white rounded-xl h-full w-full order-last lg:order-first">
-                {/* <TestsList /> */}
-            </div>
-            <div className="flex flex-wrap col-span-3 bg-white rounded-xl lg:col-span-2  h-full w-full justify-center order-first lg:order-last">
-
-                <div className='items-stretch flex flex-col gap-4 h-full w-full py-2 px-6'>
-                    <CreateTest />
+        <div className="overflow-y-auto no-scrollbar items-center w-full h-screen bg-white rounded-xl">
+            <div className='grid grid-cols-4 h-full w-full lg:divide-x-2 p-4 lg:p-0'>
+                <div className="col-span-4 lg:col-span-1 gap-4 bg-transparent lg:bg-[#e9e9e9]
+                    h-full w-full order-last lg:order-first p-4">
+                    <TestsList />
                 </div>
-
+                <div className="flex flex-wrap col-span-4 lg:col-span-3 h-full w-full
+                    justify-center order-first lg:order-last">
+                    <div className='items-stretch flex flex-col gap-4 h-full w-full p-4 '>
+                        <CreateTest />
+                    </div>
+                </div>
             </div>
         </div>
     );
