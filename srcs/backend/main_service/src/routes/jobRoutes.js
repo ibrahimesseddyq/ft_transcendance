3cb495cfcfa0a1adfe252989f04c4863c327dbe0
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-02-18 23:40:44 +0000

    refactor lagacy code for user and add some inhencments

--- srcs/backend/main_service/src/routes/jobRoutes.js
+++ srcs/backend/main_service/src/routes/jobRoutes.js
@@ -1,15 +1,24 @@
 import * as jobController from '../controllers/jobController.js';
 import express from 'express';
+import  {UserRole} from '../../generated/prisma/index.js'
 import {createJobSchema, updateJobSchema} from '../validators/jobValidator.js';
 import validateRequest from '../middleware/ValidateRequest.js';
 
 const router = express.Router();
 
-router.post('/',validateRequest(createJobSchema),jobController.createJob);
+router.post('/',
+    validateRequest(createJobSchema),
+    verifyRoles([UserRole.recruiter,UserRole.admin]),
+    jobController.createJob);
 router.get('/',jobController.getJobs);
 router.get('/:id',jobController.getJobById);
-router.patch('/:id',validateRequest(updateJobSchema),jobController.updateJob);
-router.delete('/:id',jobController.deleteJob);
+router.patch('/:id',
+    validateRequest(updateJobSchema),
+    verifyRoles([UserRole.recruiter,UserRole.admin]),
+    jobController.updateJob);
+router.delete('/:id',
+    verifyRoles([UserRole.recruiter,UserRole.admin]),
+    jobController.deleteJob);
 router.get('/:id/applications',jobController.getApplicationsByJobId);
 
 export default router;
