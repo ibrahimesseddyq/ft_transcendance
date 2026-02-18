8da38b1b157ad80cf36f8b8029320fd621038207
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-02-18 18:01:21 +0000

    add quis service to main_servic

---
+++ srcs/backend/main_service/src/services/quizService.js
@@ -0,0 +1,9 @@
+import quizClient from "../utils/quizClinet";
+
+export const startTest = async (testId) => {
+    
+}
+
+export const submitTest = async (testId, answers) => {
+    
+}
---
+++ srcs/backend/main_service/src/utils/quizClinet.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import env from '../config/env.js';
+import { HttpException } from '.httpExceptions.js';
+
+const quizClient = axios.create({
+    baseURL: env.QUIZ_SERVICE_URL,
+    timeout: 5000,
+    headers: {
+        "Content-Type" : "application/json",
+        'x-internal-api-key': env.INTERNAL_API_KEY
+    }
+})
+
+quizClient.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response)
+            throw new HttpException(error.response.status, 
+            error.response.data?.message || "quiz service error");
+        else if (error.code === 'ECONNREFUSED')
+            throw new HttpException(503, 'Quiz service is unavailable');
+        else
+            throw new HttpException(502,'Failed to communicate with quiz service')
+    }
+)
+
+export default quizClient;
