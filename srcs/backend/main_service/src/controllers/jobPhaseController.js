40b5409bc7bbbe22c9ec4c8001010f3a68f0d258
soufiane essarhir <soufianeessarhir@gmail.com>
2026-02-18 18:01:21 +0000

    add quis service to main_servic

--- srcs/backend/main_service/src/controllers/jobPhaseController.js
+++ srcs/backend/main_service/src/controllers/jobPhaseController.js
@@ -1,63 +1,43 @@
 import * as jobPhaseService from '../services/jobPhaseService.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
-export const createJobPhase = async (req, res, next) => {
-	try {
-		const jobPhase = await jobPhaseService.createJobPhase(req.body);
-		res.status(201)
-		.json({
-			success: true,
-			data:jobPhase
-		});
+export const createJobPhase = asyncHandler( async (req, res, next) => {
+	const jobPhase = await jobPhaseService.createJobPhase(req.body);
+	res.status(201)
+	.json({
+		success: true,
+		data:jobPhase
+	});
+})
 
-	} catch (error) {
-		next(error)
-	}
-}
+export const updateJobPhase = asyncHandler( async (req, res, next) => {
+	const jobPhase = await jobPhaseService.updateJobPhase(req.params.id,req.body);
+	res.status(200)
+	.json({
+		success: true,
+		data: jobPhase
+	})
+})
 
-export const updateJobPhase = async (req, res, next) => {
-	try {
-		const jobPhase = await jobPhaseService.updateJobPhase(req.params.id,req.body);
-		res.status(200)
-		.json({
-			success: true,
-			data: jobPhase
-		})
-	} catch (error) {
-		next(error);
-	}
-}
+export const getJobPhaseById = asyncHandler( async (req, res, next) => {
+	const jobPhase = await jobPhaseService.getJobPhaseById(req.params.id);
+	res.status(200)
+	.json({
+		success: true,
+		data: jobPhase
+	})
+})
 
-export const getJobPhaseById = async (req, res, next) => {
-	try {
-		const jobPhase = await jobPhaseService.getJobPhaseById(req.params.id);
-		res.status(200)
-		.json({
-			success: true,
-			data: jobPhase
-		})
-	} catch (error) {
-		next(error)
-	}
-}
+export const deleteJobPhase = asyncHandler( async (req, res, next) => {
+	await jobPhaseService.deleteJobPhase(req.params.id);
+	res.status(204);
+})
 
-export const deleteJobPhase = async (req, res, next) => {
-	try {
-		await jobPhaseService.deleteJobPhase(req.params.id);
-		res.status(204);
-	} catch (error) {
-		next(error);
-	}
-}
-
-export const getJobPhases = async(req, res, next) => {
-	try {
-		const result = await jobPhaseService.getJobPhases(req.params.id);
-		res.status(200)
-		.json({
-			success: true,
-			data: result
-		})
-	} catch (error) {
-		next(error)
-	}
-}
+export const getJobPhases = asyncHandler( async(req, res, next) => {
+	const result = await jobPhaseService.getJobPhases(req.params.id);
+	res.status(200)
+	.json({
+		success: true,
+		data: result
+	})
+})
--- srcs/backend/main_service/src/controllers/twoFAController.js
+++ srcs/backend/main_service/src/controllers/twoFAController.js
@@ -1,23 +1,22 @@
 import  TwoFAService from  '../services/twoFAService.js';
 import * as userService from  '../services/userService.js';
-
+import asyncHandler from '../utils/asyncHandler.js';
 const twoFAService = new TwoFAService(userService);
 
-export const    setup = async (req, res) =>
-    {
-        // to validate
-        const data = await twoFAService.setup(req.body.id);
-        res.json(data);
-    };
+export const    setup = asyncHandler( async (req, res) => {
+    // to validate
+    const data = await twoFAService.setup(req.body.id);
+    res.json(data);
+})
 
-export const    verifySetup = async (req, res) => {
-        const { token } = req.body;
-        const data = await twoFAService.verifySetup(req.body.id, token);
-        res.json(data);
-    };
+export const    verifySetup = asyncHandler( async (req, res) => {
+    const { token } = req.body;
+    const data = await twoFAService.verifySetup(req.body.id, token);
+    res.json(data);
+})
 
-export const    disable = async (req, res) => {
-        const { token } = req.body;
-        const data = await twoFAService.disable(req.body.id, token);
-        res.json(data);
-};
+export const    disable = asyncHandler( async (req, res) => {
+    const { token } = req.body;
+    const data = await twoFAService.disable(req.body.id, token);
+    res.json(data);
+})
