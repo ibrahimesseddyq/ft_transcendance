820356e686ffd48ef624ef8be8656ae83b87e8e3
Ibra <ibrahim.essedyq@gmail.com>
2026-02-23 03:01:47 +0000

    add recruiter

--- srcs/backend/main_service/src/app.js
+++ srcs/backend/main_service/src/app.js
@@ -21,7 +21,7 @@ const app =  express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH "],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true 
   }));
 app.use(helmet());
