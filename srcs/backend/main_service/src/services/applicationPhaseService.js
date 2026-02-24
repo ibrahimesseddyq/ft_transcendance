da4920424625a85a4dc8deaf10942a4be9cfd3ff
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-02-24 14:25:08 +0000

    fix application logic and a some protection

--- srcs/backend/main_service/src/services/applicationPhaseService.js
+++ srcs/backend/main_service/src/services/applicationPhaseService.js
@@ -1,34 +1,18 @@
 import * as applicationPhaseRepository from '../repositories/applicationPhaseRepository.js';
 import {HttpException} from '../utils/httpExceptions.js';
 
-export const createApplicationphase = async (data) => {
-    const {applicationId, phaseId} = data;
-    try {
-        const applicationPhase = await applicationPhaseRepository.createApplicationPhase({
-            applicationId: applicationId,
-            phaseId:phaseId,
-            notes: "",
-            score: 0,
-        });
-         return applicationPhase;
-    } catch (error) {
-        if (error.code === "P2002")
-            throw new HttpException(409,'application phase already exists');
-        else if (error.code === "P2003")
-            throw new HttpException(404, 'applicatioId or phaseId not valid');
-        else 
-            throw error;
-    }
+export const createApplicationPhase = async (data) => {
+    return await applicationPhaseRepository.createApplicationPhase({
+        applicationId: data.applicationId,
+        phaseId: data.phaseId,
+    });
 }
 
 export const updateApplicationPhase = async (applicationPhaseId, updateData) => {
-    const applicationPhase =  await applicationPhaseRepository.updateApplicationPhase(
+    return  await applicationPhaseRepository.updateApplicationPhase(
         applicationPhaseId,
         updateData
     );
-    if (!applicationPhase)
-        throw new HttpException(404, 'application Phase not found');
-    return applicationPhase;
 }
 
 export const getApplicaticationPhaseById =  async (applicationPhaseId) => {
