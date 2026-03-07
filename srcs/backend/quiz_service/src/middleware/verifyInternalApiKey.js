c9586b7ae5d131dbd7023847497c8a016a6d8c38
Chidori42 <137655886+Chidori42@users.noreply.github.com>
2026-03-07 03:04:12 +0000

    fix some problems

--- srcs/backend/quiz_service/src/middleware/verifyInternalApiKey.js
+++ srcs/backend/quiz_service/src/middleware/verifyInternalApiKey.js
@@ -3,7 +3,9 @@ import crypto from 'crypto';
 import env from '../config/env.js';
 
 export const verifyInternalApiKey = async (req, res, next) => {
-    let apiKey = req.headers["x-api-key"] || "";
+    const apiKey = req.headers["x-internal-api-key"] || "";
+
+    console.log("req.headers : ", req.headers);
     const isEqual = apiKey.length === env.INTERNAL_API_KEY.length;
     if(!apiKey || apiKey.length !== env.INTERNAL_API_KEY.length)
         apiKey = env.INTERNAL_API_KEY;
