8a18133344e973824784a1af40141a9bdcbfa11e
Ibrahim Es-seddyq <ibes-sed@e2r9p12.1337.ma>
2026-03-12 05:56:46 +0000

    waf modsec fix

--- srcs/backend/main_service/prisma/seed.js
+++ srcs/backend/main_service/prisma/seed.js
@@ -2,13 +2,13 @@
 import { PrismaClient, UserRole } from '../generated/prisma/index.js';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import argon2 from 'argon2';
-
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL || 'mysql://root:root@localhost:3307/hirefy');
+// TODO !!!1 use just env var and remove plainpassword
+const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  const plainPassword = 'Abdellatif123@@';
-  const passwordHash = await argon2.hash(plainPassword);
+  const password = process.env.RECRUITER_PASS;
+  const passwordHash = await argon2.hash(password);
 
   const recruiter = await prisma.user.upsert({
     where: { email: 'recruiter@company.com' },
@@ -29,7 +29,7 @@ async function main() {
     id: recruiter.id,
     email: recruiter.email,
     role: recruiter.role,
-    password: plainPassword,
+    password: password,
   });
 }
 
