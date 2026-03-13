36985fa6364ec85fc219fbe050b62c4d701940d3
soufianeessarhir <soufianeessarhir@gmail.com>
2026-03-13 03:58:22 +0000

    fix error in saveAvatar function

--- srcs/backend/main_service/src/config/env.js
+++ srcs/backend/main_service/src/config/env.js
@@ -47,7 +47,6 @@ const envSchema = z.object({
   BACKEND_URL:z.string(),
   APP_NAME:z.string().min(1).default("service"),
   QUIZ_SERVICE_URL: z.string(),
-  AI_INTERNAL_API_KEY:z.string(),
   INTERNAL_API_KEY: z.string(),
   AI_SERVICE_URL: z.string(),
   AI_INTERNAL_API_KEY: z.string(),  
