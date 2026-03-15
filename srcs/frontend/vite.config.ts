c2bfc420024a00d9831dade58db2e01e96bff4c0
elfagrouch9 <elfagrouch9@gmail.com>
2026-03-15 06:53:18 +0000

    merge

--- srcs/frontend/vite.config.ts
+++ srcs/frontend/vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  server:{
+    port: 5173,
+    strictPort: true,
+  },
   plugins: [
     react(),
   ],
