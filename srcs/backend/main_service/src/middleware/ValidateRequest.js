4b60b68c65e23d81ebce60ab4a8630cf00ba4c48
Soufiane Essarhir <soufianeessarhir@gmail.com>
2026-03-15 07:18:27 +0000

    fix chat errors in backend side

--- srcs/backend/main_service/src/middleware/ValidateRequest.js
+++ srcs/backend/main_service/src/middleware/ValidateRequest.js
@@ -1,9 +1,10 @@
-import  { ZodError } from 'zod';
+import  {z, ZodError } from 'zod';
 import  { HttpValidationException } from '../utils/httpExceptions.js';
 
-const validateRequest = (schema, target = 'body') => {
+const validateRequest = (schema) => {
     return (req,res,next) => {
         try {
+            console.log('req.body validate request middelware ',req.body)
             req.body = schema.parse(req.body);
         } catch (error) {
             if(error instanceof ZodError) {
