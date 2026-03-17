d75483430842d860c0fe97d92335d558ee3a8a1a
AM9-push <achllahabdelmajid@gmail.com>
2026-03-17 13:15:12 +0000

    all ai services is working

--- srcs/backend/ai_services/ai_service/app/main.py
+++ srcs/backend/ai_services/ai_service/app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import (
     img_classf_router,
-    llm_rag_router,
     speech_recognition_router,
     text_moderation_router,
 )
@@ -26,8 +25,3 @@ app.include_router(img_classf_router.router)
 app.include_router(text_moderation_router.router)
 
 app.include_router(speech_recognition_router.router)
-
-app.include_router(llm_rag_router.router)
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="127.0.0.1", port=3002, reload=True)
--- srcs/backend/ai_services/rag_service/app/routers/llm_rag_router.py
+++ srcs/backend/ai_services/rag_service/app/routers/llm_rag_router.py
@@ -1,8 +1,12 @@
-from fastapi import APIRouter, HTTPException, Request
+import os
+
+from dotenv import load_dotenv
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.concurrency import run_in_threadpool
+from jose import JWTError, jwt
 from limiter.limiter import limiter
 from pydantic import BaseModel
-from services.llm_rag.generate import generate
+from services.generate import generate
 
 router = APIRouter()
 
@@ -11,9 +15,30 @@ class GenerationRequest(BaseModel):
     text: str
 
 
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_TOKEN")
+
+
+def jwt_verify(request: Request):
+
+    token = request.headers.get("Cookie")
+
+    if token is None:
+        raise HTTPException(status_code=401, detail="Missing token")
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+
 @router.post("/api/ai/generate")
 @limiter.limit("2/minute")
-async def generation_endpoint(request: Request, body: GenerationRequest):
+async def generation_endpoint(
+    request: Request, body: GenerationRequest, user=Depends(jwt_verify)
+):
 
     try:
         result = await run_in_threadpool(generate, body.text)
