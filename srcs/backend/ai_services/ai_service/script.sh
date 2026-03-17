e95fb2b0d5b8f0518ddfa46d898a808b2f255116
ibrahim.essedyq <ibrahim.essedyq@gmail.com>
2026-03-17 18:08:04 +0000

    test ai

--- srcs/backend/ai_services/ai_service/script.sh
+++ srcs/backend/ai_services/ai_service/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /ai_service/app
+cd /app
 
-/root/.local/bin/uv run uvicorn main:app --port 3002 --host 0.0.0.0 --reload
+/root/.local/bin/uv run uvicorn main:app --port 3002 --host 0.0.0.0 
--- srcs/backend/ai_services/rag_service/script.sh
+++ srcs/backend/ai_services/rag_service/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /ai_service/app
+cd /app
 
 . /.venv/bin/activate
 
-/root/.local/bin/uv run uvicorn main:app --port 3003 --host 0.0.0.0 --reload --workers 4
+/root/.local/bin/uv run uvicorn main:app --port 3003 --host 0.0.0.0  --workers 4
