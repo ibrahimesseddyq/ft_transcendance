129eed5b673fa1e7cf63e4e7beedb9c3935b2d29
ibrahim.essedyq <ibrahim.essedyq@gmail.com>
2026-03-18 18:06:23 +0000

    fix secret ai

--- srcs/backend/ai_services/ai_service/script.sh
+++ srcs/backend/ai_services/ai_service/script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 . /app/.venv/bin/activate
-
+. /vault/secrets/config
+. /vault/secrets/jwt
+. /vault/secrets/other
 cd /app/app
 /app/.venv/bin/uvicorn main:app --port 3002 --host 0.0.0.0
