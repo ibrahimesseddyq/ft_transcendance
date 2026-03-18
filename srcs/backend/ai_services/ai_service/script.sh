393c86f4818eac3a5678a58ebec3a46e35fb8e0f
ibrahim.essedyq <ibrahim.essedyq@gmail.com>
2026-03-18 02:43:14 +0000

    fix ai svc

--- srcs/backend/ai_services/ai_service/script.sh
+++ srcs/backend/ai_services/ai_service/script.sh
@@ -3,5 +3,4 @@
 . /app/.venv/bin/activate
 
 cd /app/app
-
-PYTHONPATH=/app/app /root/.local/bin/uv run uvicorn main:app --port 3002 --host 0.0.0.0
+/app/.venv/bin/uvicorn main:app --port 3002 --host 0.0.0.0
